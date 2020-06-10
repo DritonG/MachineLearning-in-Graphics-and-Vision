0d36bd1e5555a3946c2189e5b359716a2fac8508
dar <del.nor@outlook.com>
2020-06-10 20:00:21 +0200

    Ex 3 - added 3.2.a)

--- Exercise_3/task_3_2_a.py
+++ Exercise_3/task_3_2_a.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pdb
 
+import torchvision
+
+
 class NoisyFashionMNIST(Dataset):
 
     def __init__(self, incoming_df):
@@ -32,8 +35,13 @@ class NoisyFashionMNIST(Dataset):
         # return a pair of data [original_image, noisy_image]
         # see Tutorial for hints
 
+        image = self.incoming_df.data[idx] / 255.0
 
-        pass
+        noise = torch.rand([28, 28], dtype=torch.float)
+
+        noisy_image = noise + image
+
+        return {'image': image, 'noisy_image': noisy_image}
 
 
 
@@ -43,8 +51,7 @@ if __name__ == '__main__':
     fmnist_train = dset.FashionMNIST("./", train=True, transform=transforms.ToTensor())
     fmnist_noisy = NoisyFashionMNIST(fmnist_train)
 
-
-
+    images = []
     for i in range(80):
 
 
@@ -52,5 +59,15 @@ if __name__ == '__main__':
         index = random.randint(100,200)
         dp = fmnist_noisy[index]
 
+        images.append(dp['noisy_image'].expand(1, 28, 28))
+        images.append(dp['image'].expand(1, 28, 28).to(dtype=torch.float))
+        
 
     #TODO: visualize (noisy_image, clean_image) in a grid
+    images_grid = make_grid(images, nrow=10)
+
+    plt.imshow(np.transpose(images_grid.numpy(), (1, 2, 0)), cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+    torchvision.utils.save_image(images_grid, '3-2-a.jpg')
